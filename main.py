e35ac693173c21da315c3ca5dd2f73a8f579b013
Alexander Emmerich <al_emmerich@gmx.de>
2021-04-04 07:56:05 +0200

    Added tile size parameter when creating image

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from image_tools import array_to_image, image_to_array, load_image
 
 def test_truchet_tile():
     size: int = 64
-    image = array_to_image(truchet_tile(size, 0, 3))
+    image = array_to_image(truchet_tile(size, 0.60, 0))
     image.save("test.png")
 
 
 def test_truchet_image():
-    file = "input/jesus.jpg"
-    out = "output/jesus.jpg"
+    name = "rick.png"
+    file = "input/" + name
+    out = "output/" + name
     arr = image_to_array(load_image(file))
-    new_img = array_to_image(truchet_image(arr, 20, [[2,3],[1,0]]))
+    new_img = array_to_image(truchet_image(arr, 3, [[2,3],[0,1]], 32))
     new_img.save(out)
 
 
 test_truchet_image()
+#test_truchet_tile()
--- truchet.py
+++ truchet.py
@@ -1,10 +1,6 @@
 import numpy as np
 from skimage.measure import block_reduce
-from typing import List
-
-
-def brightness_value(arr: np.ndarray):
-    return arr.mean() / 255.
+from typing import List, Optional
 
 
 def optimal_point(brightness: float) -> float:
@@ -19,7 +15,7 @@ def optimal_point(brightness: float) -> float:
 def color_of_tile(x: float, y: float, mid_point: float) -> int:
     is_in_area1: bool = y < (mid_point - 1) / mid_point * x + 1
     is_in_area2: bool = y < mid_point / (1 - mid_point) * (1 - x)
-    if mid_point < 0.5:
+    if mid_point <= 0.5:
         return 255 * (int(is_in_area1 or is_in_area2))
     else:
         return 255 * (int(is_in_area1 and is_in_area2))
@@ -38,7 +34,9 @@ def truchet_tile(size: int, brightness: float, rotation: int = 0):
     return ret
 
 
-def truchet_image(arr, size, pattern: List[List[int]]):
+def truchet_image(arr, size, pattern: List[List[int]], tile_size: Optional[int] = None):
+    if tile_size is None:
+        tile_size = size
     brightness_arr = block_reduce(arr, (size, size), np.mean) / 255.
     new_mat = []
     n: int = len(pattern)
@@ -46,7 +44,7 @@ def truchet_image(arr, size, pattern: List[List[int]]):
     for i, row in enumerate(brightness_arr):
         new_row = []
         for j, column in enumerate(row):
-            new_row.append(truchet_tile(size, column, pattern[i % n][j % m]))
+            new_row.append(truchet_tile(tile_size, column, pattern[i % n][j % m]))
         new_mat.append(new_row)
     return np.block(new_mat)
 
